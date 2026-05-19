dfc58d3926918b1043c630b02d6aba597f4f6058
Matthew <mattkho922@gmail.com>
2026-05-19 17:15:52 -0400

    put generic API in config file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -19,12 +19,12 @@ import { getStorage } from 'firebase/storage';
 import { Platform } from 'react-native';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhzEcZjNGbhRlALu-WIsorCPfd1SOaNPY",
-  authDomain: "fitness-app-9bd83.firebaseapp.com",
-  projectId: "fitness-app-9bd83",
-  storageBucket: "fitness-app-9bd83.firebasestorage.app",
-  messagingSenderId: "673022799784",
-  appId: "1:673022799784:web:94475cfc71e318690b379d"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
