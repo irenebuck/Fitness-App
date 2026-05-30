4dcb143ab78b075b7a82ecd7450781c4425c6b54
Irene Buck <irenedevito@gmail.com>
2026-05-29 22:27:30 -0700

    Added README, updated package.json and package-lock.json, completed HomeScreen and LoginScreen tests

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,9 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { Platform } from 'react-native';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
@@ -14,7 +16,12 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+export const auth = Platform.OS === 'android'
+  ? initializeAuth(app, {
+      persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+    })
+  : getAuth(app);
+
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
